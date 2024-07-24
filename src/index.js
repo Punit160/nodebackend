27f7c71722197f04adc7df822aa9661eedb4bb12
Punit Sukhija <punitsukhija@Punits-Laptop.local>
2024-07-24 12:43:32 +0530

    new updates

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// require('dotenv').config({path : './env'})
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import { app } from './app.js'
+
+
+dotenv.config({
+    path : '/.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running at ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log('Database Connection error !!!', err)
+})
+
+
+/*
+import express from 'express'
+
+
+const app = express()
+
+(async() => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+       app.on("error", (error) => {
+          console.log("error : ", erorr)
+          throw error
+       })
+
+       app.listen(process.env.PORT , () => {
+        console.log(`App is listening on ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.error('Error :', error)
+        throw error
+    }
+
+})()
+
+*/
